9dd2f3b4e5c962ecd5e7b6b4b1b82811d14f1ff5
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-10-02 20:56:19 -0700

    setup apollo server with graphql api

--- server/server.js
+++ server/server.js
@@ -1,15 +1,38 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3333;
 const app = express();
 
+const startServer = async () => {
+    // create a new Apollo server and pass in our schema data
+    const server = new ApolloServer({ 
+      typeDefs, 
+      resolvers
+    });
+  
+    // Start the Apollo server
+    await server.start();
+  
+    // integrate our Apollo server with the Express application as middleware
+    server.applyMiddleware({ app });
+  
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+  
+// Initialize the Apollo server
+startServer();
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-app.use('/images', express.static(path.join(__dirname, '../client/images')));
+// app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
@@ -21,9 +44,9 @@ app.get('/', (req, res) => {
     });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 // Start server after DB connection
 db.once('open', () => {
