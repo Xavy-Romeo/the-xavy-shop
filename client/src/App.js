bb9f74fb8c27d79a093e8fe8a430e89cb01b702b
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-10-02 21:19:19 -0700

    initial react setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div>
+      HELLO WORLD
+      <h1>
+        THE XAVY SHOP!!!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
