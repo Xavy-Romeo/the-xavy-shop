8684bde9ff4cb2a541753e88041045c8cf682f25
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-10-27 12:33:06 -0700

    cleared cart on purchase and added order to order history

--- client/src/pages/SuccessOrder/index.js
+++ client/src/pages/SuccessOrder/index.js
@@ -1,15 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useMutation } from '@apollo/client';
 
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 
 import useStyles from './styles';
+import { ADD_ORDER } from '../../utils/mutations';
 import Logo from '../../assets/images/xr-logo.png';
+import idbPromise from '../../utils/indexedDB';
 
 const SuccessOrder = () => {
     const classes = useStyles();
 
     const [countDown, setCountDown] = useState(8);
+
+    const [addOrder] = useMutation(ADD_ORDER);
+
+    useEffect(() => {
+        const saveOrder = async () => {
+            // get cart data from IndexedDB
+            const cart = await idbPromise('cart', 'get');
+            const products = cart.map(item => item._id);
+
+            if (products.length) {
+                // add order to user order history array via ADD_ORDER mutation
+                const { data } = await addOrder({ 
+                    variables: { products } 
+                });
+                const productData = data.addOrder.products;
+
+                // remove items from cart
+                productData.forEach((item) => {
+                    idbPromise('cart', 'delete', item);
+                });
+            }
+
+            setTimeout(() => {
+                window.location.assign('/');
+            }, 8000);
+        };
+
+        saveOrder();
+    }, [addOrder]);
 
     setTimeout(() => {
         if (countDown > 0) {
@@ -17,10 +49,6 @@ const SuccessOrder = () => {
             setCountDown(count);
         }
     }, 1000);
-
-    setTimeout(() => {
-        window.location.assign('/');
-    }, 8000);
 
     return (
         <Box className={classes.pageContainer_SuccessOrder}>
