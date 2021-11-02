e503ec038acf24d28a76e1bc76e769c703b31498
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-11-01 23:10:36 -0700

    fixed account page alignment

--- client/src/pages/Account/styles.js
+++ client/src/pages/Account/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({ 
     accountPageContainer_Account: {
         marginTop: '120px',
-        marginBotton: '150px'
+        marginBottom: '150px'
     },
     flexBox_Account: {
         display: 'flex'
